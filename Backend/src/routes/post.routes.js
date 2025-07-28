4b00140488fca31bc4987d18e198c3d389192df6
Dev Sharma <devsharma1683@gmail.com>
2025-07-28 21:02:44 +0530

    setup an route for which returns the caption and takes an image file

--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer')
 const uploadFile = require('../service/storage.service')
 const Post = require('../models/post.model')
+const generateCaption = require('../service/aiCaption.service')
 
 const router = express.Router()
 const upload = multer({storage: multer.memoryStorage()})
@@ -20,7 +21,8 @@ router.post('/upload',upload.single("image"),async(req,res)=>{
     const post = await Post.create({
         userId: decodedToken.id,
         imageUrl: fileData.url,
-        imageId: fileData.fileId
+        imageId: fileData.fileId,
+        caption: req.body.caption
     })
     
     res
@@ -42,6 +44,26 @@ router.get("/allposts",async(req,res)=>{
         message: "User posts fetched successfully",
         data: posts
     });
+})
+
+router.post('/generate-captions',upload.single("image"),async(req,res)=>{
+    const token = req.cookies.user
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+    if(!decodedToken) return res.status(400).json({message: "Unauthorized token, login first.!"})
+    
+    const file = req.file;
+
+    const caption = await generateCaption(file.buffer, file.mimetype)
+    if(!caption || caption === "Failed to generate caption.") {
+        return res.status(400).json({message: "Caption could not be generated"})
+    }
+
+    res.status(200)
+    .json({
+        message:"Generated Caption:",
+        caption: caption
+    })
 })
 
 module.exports = router
